38922ce099f29eb477e1cbba06dba468226e4d19
sakura-pc <841105127@qq.com>
2016-09-25 22:26:55 +0800

    data bind

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,5 +1,14 @@
+var aboutData={
+  text:'ceshi'
+}
 Page({
+    data:aboutData,
     getout:function() {
         wx.navigateBack()
+    },
+    inputchange:function(e) {
+        this.setData({
+            text:e.detail.value
+        })
     }
 })
